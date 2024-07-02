48ef7797dfadd99421beb0771d73d08bcaeff9b1
Yujian Yao <yujian@yujians-mbp.local.meter>
2024-07-02 16:58:03 -0700

    fix storage pane

--- frontend/src/generate_screen_layout.ts
+++ frontend/src/generate_screen_layout.ts
@@ -1,86 +1,96 @@
 type ScreenLayoutItem = {
-    element: HTMLElement
-    itemId: number
-    top: number
-    left: number
-    width: number
-    height: number
-    backgroundColor?: string
-    text?: string
-}
+  element: HTMLElement;
+  itemId: number;
+  top: number;
+  left: number;
+  width: number;
+  height: number;
+  backgroundColor?: string;
+  text?: string;
+};
 
-const IncludedTags = new Set([
-    "BUTTON",
-    "A",
-    "INPUT",
-    "SELECT",
-    "TEXTAREA"
-])
+const IncludedTags = new Set(["BUTTON", "A", "INPUT", "SELECT", "TEXTAREA"]);
 
 function walkDOM(dom: HTMLElement, process: (child: HTMLElement) => boolean) {
-    dom.childNodes.forEach(n => {
-        if (n instanceof HTMLElement && process(n)) {
-            walkDOM(n, process)
-        }
-    })
+  dom.childNodes.forEach((n) => {
+    if (n instanceof HTMLElement && process(n)) {
+      walkDOM(n, process);
+    }
+  });
 }
 
-function elementToInfo(element: HTMLElement, rect: DOMRect, styles: CSSStyleDeclaration, id: number): ScreenLayoutItem {
-    const info: ScreenLayoutItem = {
-        itemId: id,
-        top: Math.round(rect.top),
-        left: Math.round(rect.left),
-        width: Math.round(rect.width),
-        height: Math.round(rect.height),
-        element,
-    }
-    const backgroundColor = styles.backgroundColor
-    if (backgroundColor && backgroundColor !== 'rgba(0, 0, 0, 0)') {
-        info.backgroundColor = backgroundColor
-    }
-    const text = element.textContent?.trim()
-    if (text) {
-        info.text = text
-    }
+function elementToInfo(
+  element: HTMLElement,
+  rect: DOMRect,
+  styles: CSSStyleDeclaration,
+  id: number
+): ScreenLayoutItem {
+  const info: ScreenLayoutItem = {
+    itemId: id,
+    top: Math.round(rect.top),
+    left: Math.round(rect.left),
+    width: Math.round(rect.width),
+    height: Math.round(rect.height),
+    element,
+  };
+  const backgroundColor = styles.backgroundColor;
+  if (backgroundColor && backgroundColor !== "rgba(0, 0, 0, 0)") {
+    info.backgroundColor = backgroundColor;
+  }
+  const text = element.textContent?.trim();
+  if (text) {
+    info.text = text;
+  }
 
-    let cur = element.parentElement
-    while (cur && cur !== document.body && cur.childElementCount === 1) {
-        const curRect = cur.getBoundingClientRect()
-        if (curRect.left !== rect.left || curRect.right !== rect.right || curRect.top !== rect.top || curRect.bottom !== rect.bottom) {
-            break;
-        }
-        if (!info.backgroundColor) {
-            const backgroundColor = window.getComputedStyle(cur).backgroundColor
-            if (backgroundColor && backgroundColor !== 'rgba(0, 0, 0, 0)') {
-                info.backgroundColor = backgroundColor
-            }
-        }
-        info.element = cur
-        cur = cur.parentElement
+  let cur = element.parentElement;
+  while (cur && cur !== document.body && cur.childElementCount === 1) {
+    const curRect = cur.getBoundingClientRect();
+    if (
+      curRect.left !== rect.left ||
+      curRect.right !== rect.right ||
+      curRect.top !== rect.top ||
+      curRect.bottom !== rect.bottom
+    ) {
+      break;
     }
+    if (!info.backgroundColor) {
+      const backgroundColor = window.getComputedStyle(cur).backgroundColor;
+      if (backgroundColor && backgroundColor !== "rgba(0, 0, 0, 0)") {
+        info.backgroundColor = backgroundColor;
+      }
+    }
+    info.element = cur;
+    cur = cur.parentElement;
+  }
 
-    return info
+  return info;
 }
 
 export function generateScreenLayout(): ScreenLayoutItem[] {
-    const result: ScreenLayoutItem[] = [];
-    let counter = 0;
+  const result: ScreenLayoutItem[] = [];
+  let counter = 0;
 
-    walkDOM(document.body, (element: HTMLElement) => {
-        if (element.attributes.getNamedItem("data-isgandalf")) {
-            return false
-        }
-        const styles = window.getComputedStyle(element)
-        const cursor = styles.cursor
-        if (element.checkVisibility() && (IncludedTags.has(element.tagName) || (element.onclick != null) || cursor == "pointer" || cursor == "grab")) {
-            const rect = element.getBoundingClientRect()
-            if (rect.width > 0 && rect.height > 0) {
-                result.push(elementToInfo(element, rect, styles, counter++))
-            }
-            return false
-        }
-        return true
-    });
+  walkDOM(document.body, (element: HTMLElement) => {
+    if (element.attributes.getNamedItem("data-isgandalf")) {
+      return false;
+    }
+    const styles = window.getComputedStyle(element);
+    const cursor = styles.cursor;
+    if (
+      element.checkVisibility() &&
+      (IncludedTags.has(element.tagName) ||
+        (element.onclick && !element.onclick.toString().endsWith("(){}")) ||
+        cursor == "pointer" ||
+        cursor == "grab")
+    ) {
+      const rect = element.getBoundingClientRect();
+      if (rect.width > 0 && rect.height > 0) {
+        result.push(elementToInfo(element, rect, styles, counter++));
+      }
+      return false;
+    }
+    return true;
+  });
 
-    return result;
+  return result;
 }
